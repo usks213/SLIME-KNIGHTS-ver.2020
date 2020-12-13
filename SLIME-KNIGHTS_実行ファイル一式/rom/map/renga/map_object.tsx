c99aa2a82c9a32d1f5dcb62aaeb860e634889e7e
usami213 <nhs90475@gmail.com>
2020-12-13 17:35:38 +0900

    Revert "U22プロコン CSJA賞 SLIME KNIGHTS"
    This reverts commit d8667cc1054c276829847510a19f2e5e4c6008d5.

--- SLIME-KNIGHTS_実行ファイル一式/rom/map/renga/map_object.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.1" name="map_object" tilewidth="16" tileheight="16" tilecount="6" columns="6">
- <image source="../../asprite/map/map_object.png" width="96" height="16"/>
-</tileset>
